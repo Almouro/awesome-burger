1db1cd6ef22d4d6b643932df2ef59084a5eb3f2a
Almouro <contact@almouro.com>
2015-06-27 17:39:40 +0200

    Lightweight gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
-var fs = require('fs');
 var replace = require('gulp-replace');
+var sass = require('gulp-sass');
+var fs = require('fs');
 
-gulp.task('insert-burger-template', [], function () {
+gulp.task('script', [], function () {
     var awesomeBurgerHtml = fs.readFileSync('src/awesome-burger.html', "utf8")
-    .replace(/(?:\r\n|\r|\n)/g, '');
+      .replace(/(?:\r\n|\r|\n)/g, '');
+
     return gulp.src(['src/js/awesome-burger.js'])
      .pipe(replace('%AWESOME_BURGER_HTML%', awesomeBurgerHtml))
      .pipe(gulp.dest("dist/js"));
 });
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['script', 'sass']);
+
+gulp.task('watch', [], function() {
+  gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
